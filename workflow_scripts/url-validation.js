943c1df81eb52564f1bbd8eb4c86b5d030ca2e40
korn101 <jaime2psp@gmail.com>
2024-01-12 09:11:56 +0200

    upgrade tldjs -> tldts

--- workflow_scripts/url-validation.js
+++ workflow_scripts/url-validation.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const tldjs = require("tldjs");
+const tldts = require("tldts");
 const { getDomainJsonFileNames } = require("./helpers/get-json-file-names");
 const { log } = require("./helpers/log");
 
@@ -13,7 +13,7 @@ const init = async () => {
         const data = JSON.parse(fs.readFileSync(dirPath));
 
         for (let url of data) {
-            if (tldjs.parse(url).hostname !== url) {
+            if (tldts.parse(url).hostname !== url) {
                 log.error(`'${url}' in "${fn}" is not a valid domain.`);
                 failed = true;
             }
